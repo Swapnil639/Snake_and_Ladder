6ffd3a33e6930f8b798ae024b9d697cae72db038
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-18 17:50:34 +0530

    Added :Single player at start position zero

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder program");
+
+        int player1position=0;
+        System.out.println("Player1 at position :"+player1position);
     }
 }
